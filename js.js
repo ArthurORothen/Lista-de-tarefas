d1dded5d7b8ae7c73b7b7624768b249412bff9ec
ArthurORothen <arthurrothen2@gmail.com>
2026-03-03 11:05:42 -0300

    Imagem de fundo e atualizações gerais

--- js.js
+++ js.js
@@ -3,8 +3,13 @@ let btRemover = document.querySelector('#btRemover')
 let inputTarefa = document.querySelector('#inputTarefa')
 let listaTarefas = document.querySelector('#listaTarefas')
 
-
 function adicionar() {
+
+    if (inputTarefa.value == "") {
+        alert("Por favor digite o nome da sua tarefa")
+        return
+    }
+
     let novaTarefa = document.createElement('li')
     let checkbox = document.createElement('input')
     let nomeTarefa = document.createElement('span')
